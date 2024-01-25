97264cde8023713cad854658578c3b345372b7a2
Nikita <nikita_gras@mail.ru>
2024-01-25 23:57:17 +0700

    Create new project

---
+++ Trackify/Trackify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Trackify
+//
+//  Created by Nikita Gras on 25.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Trackify/TrackifyUITests/TrackifyUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TrackifyUITestsLaunchTests.swift
+//  TrackifyUITests
+//
+//  Created by Nikita Gras on 25.01.2024.
+//
+
+import XCTest
+
+final class TrackifyUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
