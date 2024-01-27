5d5e9d0f3c746b4330f0fe7bbeefddcf226e6831
Nikita <nikita_gras@mail.ru>
2024-01-27 22:05:26 +0700

    Add SnapKit

---
+++ Trackify/Pods/Target Support Files/Pods-Trackify-TrackifyUITests/Pods-Trackify-TrackifyUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Trackify_TrackifyUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Trackify_TrackifyUITests
+@end
---
+++ Trackify/Pods/Target Support Files/Pods-TrackifyTests/Pods-TrackifyTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TrackifyTests : NSObject
+@end
+@implementation PodsDummy_Pods_TrackifyTests
+@end
